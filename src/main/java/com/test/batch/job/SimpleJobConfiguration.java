b68d79f12ad544710dc03ab4c735395290705108
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-11-01 15:46:06 +0900

    @Autowried로 job 설정

--- src/main/java/com/test/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/test/batch/job/SimpleJobConfiguration.java
@@ -5,18 +5,22 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class SimpleJobConfiguration {
 	
+	@Autowired
 	private final JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
 	private final StepBuilderFactory stepBuilderFactory;
 	
 	@Bean
@@ -26,7 +30,8 @@ public class SimpleJobConfiguration {
 	
 	@Bean
 	public Step simpleStep1() {
-		return stepBuilderFactory.get("simpleStep1").tasklet((contribution,chunkContext)->{log.info("step1"); return RepeatStatus.FINISHED;}).build();
+		return stepBuilderFactory.get("simpleStep1").tasklet((contribution,chunkContext)->{System.out.println("firststep"); return RepeatStatus.FINISHED;}).build();
 	}
+	
 
 }
