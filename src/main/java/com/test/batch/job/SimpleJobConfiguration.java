8bfd10a0611bc8f7a75b2e0dce3edef7af9a5a02
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-11-02 14:29:54 +0900

    @Autowired 삭제 및 코드 정리

--- src/main/java/com/test/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/test/batch/job/SimpleJobConfiguration.java
@@ -3,6 +3,7 @@ package com.test.batch.job;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,20 +18,38 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 public class SimpleJobConfiguration {
 	
-	@Autowired
 	private final JobBuilderFactory jobBuilderFactory;
-	
-	@Autowired
 	private final StepBuilderFactory stepBuilderFactory;
 	
 	@Bean
 	public Job simpleJob() {
-		return jobBuilderFactory.get("simpleJob").start(simpleStep1()).build();
+		return jobBuilderFactory
+				.get("simpleJob")
+				.start(simpleStep1())
+				.next(simpleStep2())
+				.build();
 	}
 	
 	@Bean
 	public Step simpleStep1() {
-		return stepBuilderFactory.get("simpleStep1").tasklet((contribution,chunkContext)->{System.out.println("firststep"); return RepeatStatus.FINISHED;}).build();
+		return stepBuilderFactory.get("simpleStep3")
+				.tasklet((contribution,chunkContext)->{
+				 System.out.println("hello world");
+				return RepeatStatus.FINISHED;
+				})
+				.build();
+		
+	}
+	
+	@Bean
+	public Step simpleStep2() {
+		return stepBuilderFactory.get("simpleStep4")
+				.tasklet((contribution,chunkContext)->{
+				 System.out.println("hello world2");
+				return RepeatStatus.FINISHED;
+				})
+				.build();
+		
 	}
 	
 
